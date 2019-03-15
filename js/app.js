bafd7b128457823f5a3d0979856339b9acb5fcdc
ChloieP <chloieparsons@gmail.com>
2019-03-15 12:44:11 -0700

    init commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var name1 = prompt('Hello! What is your name?');
+console.log(name1);
+alert(name1 + ', nice to meet you!');
+
+var veggie = prompt('What is your favorite vegetable?');
+console.log(veggie);
+alert(name1 + ', eating ' + veggie + ' is good for you.');
+
+var car = prompt('What is your favorite car?');
+console.log(car);
+alert(name1 + ', eating ' + veggie + ' while driving your ' + car + ' may not be safe. Accidents are expensive!');
+
+var wreck = prompt('Do you like lawyers?');
+console.log(wreck);
+alert(name1 + ', lawyers love people who eat ' + veggie + ' while driving their ' + car + '.');
+
+var veggie = prompt('What is your favorite vegetable?');
+console.log(veggie);
+alert(name1 + ', eating ' + veggie + ' is good for you.');
