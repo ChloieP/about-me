c03c7a722f13e2c1e77a8bca81fcde0b8701f679
ChloieP <chloieparsons@gmail.com>
2019-03-20 18:18:40 -0700

    Got questions 6 and 7 coded and functional, added score codeng  to calculate total wins versus total questions. Next up is some CSS and HTML work.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var score = 0;
 
 // eslint-disable-next-line no-redeclare
 var name = prompt('Hello! What is your name?');
@@ -9,6 +10,7 @@ var likesSpidersSnakes = prompt('I really like spiders and snakes. Do you agree?
 
 if(likesSpidersSnakes === 'no' || likesSpidersSnakes === 'n') {
   alert(name + ', you are right! I prefer ladybugs and leave snakes alone.');
+  score++;
 } else if (likesSpidersSnakes === 'yes' || likesSpidersSnakes === 'y'){
   alert(name + ', I do NOT like spiders - dead or alive, and am very cautious around snakes.');
 }
@@ -18,6 +20,7 @@ var likesBeach = prompt('I would rather be at the beach. Do you agree?').toLower
 
 if(likesBeach === 'yes' || likesBeach === 'y') {
   alert(name + ', you are right! I would rather be at the beach in a hammock.');
+  score++;
 } else if(likesBeach === 'no' || likesBeach ==='n'){
   alert(name + ', seriously, I am in an island state of mind most of the time.');
 }
@@ -27,6 +30,7 @@ var likesFishing = prompt('Do you believe that I love to go fishing any time of 
 
 if(likesFishing === 'yes' || likesFishing === 'y') {
   alert(name + ', you got it right! I enjoy fishing in sunlight or snow but not in the rain.');
+  score++;
 } else if(likesFishing === 'no' || likesFishing === 'n') {
   alert(name + ', guess you do not know me very well. I love fishing!');
 }
@@ -36,6 +40,7 @@ var yesBrain = prompt('Can you believe I am nearly 50/50 right brain/left brain?
 
 if(yesBrain === 'yes' || yesBrain === 'y') {
   alert(name + ', I can see you know me well. Just a few more questions left!');
+  score++;
 } else if(yesBrain === 'no' || yesBrain === 'n') {
   alert(name + ', surprise! I am a mixed media artist, software developer, and graphic designer! WOW!');
 }
@@ -47,6 +52,7 @@ if(noXRayVision === 'yes' || noXRayVision === 'y') {
   alert(name + ', REALLY??? Who do you think I am?');
 } else {
   alert(name + ', you are right! My superpowers include time management and courage.');
+  score++;
 }
 console.log('I do not have xray vision.');
 
@@ -55,26 +61,33 @@ var rounds = 1;
 
 if(manyStates === 49) {
   alert(name + ', WOW! You are really good at this game! Congratulations, you are correct!!!');}
+score++;
 while (manyStates !==49 && rounds <= 4) {
   manyStates = parseInt(prompt(name + ', keep trying! That answer was too low or too high.'));
   rounds++;
 }
 console.log('manyStates works!');
 
-var thanks = ('Thank you for playing! Goodbye, ' + name + ', it was fun to play with you today!');
+var favColors = ['orange', 'purple', 'turquoise'];
+var roundsColor = 1;
+var forceTrue = true;
+
+while(forceTrue === true){
+  var guessColors = prompt('Are you able to name at least one of my 3 favorite colors. You have 6 attempts.').toLowerCase();
+  if(guessColors === favColors [0] || guessColors === favColors [1] || guessColors === favColors [2]){
+    alert(name + ', I am amazed you guessed the right color! My favorite 3 colors are highly saturated orange, purple, and turquoise.');
+    score++;
+    forceTrue = false;
+  }
+  else if (roundsColor >= 6)
+    alert('Sorry, you lose! My favorite 3 colors are highly saturated orange, purple, and turquoise.');
+  roundsColor++;
+  forceTrue = false;
+}
+
+console.log(favColors[0]);
+console.log(favColors[2]);
+
+var thanks = ('Thank you for playing! Today, ' + name + ', you scored ' + score + ' out of 7!');
 console.log('Thanks for playing About Me: Chloie!');
 alert(thanks);
-
-// var favColors = prompt('Are you able to name at least one of my 3 favorite colors. You have 6 attempts.');
-// var roundsColor = 1;
-// // Copied from above and applied string values for fav colors, but no logical code yet.
-// if(favColors === 'Orange' || favColors === 'Purple' || favColors === 'Turquoise'); {
-//   alert(name + ', that is one of my favorite colors! I love highly saturated orange, purple, and turquoise.');
-// } else if(favColors === 'Does not GUESS RIGHT') {
-//   while (roundsColor <= 6) {
-//     alert(name + ', you get to learn something new about me: I love highly saturated orange, purple, and turquoise.');
-//     prompt('Keep trying to guess how many states I have lived in. Good luck!');
-//     roundsColor++;
-//   }
-// }
-// console.log('favColors works!');
